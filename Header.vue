585ef27c91f6a63f718e5051954c056ad2545138
Hisashi Iwamoto <hisasi622@gmail.com>
2020-07-17 08:10:25 +0900

    firstcommit

---
+++ Header.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+.header
+  Slidshow
+  header
+    h1 Cresta Design
+    ul: li(v-for="item in items") {{ item.list }}
+</template>
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { list: 'Concept' },
+        { list: 'Service' },
+        { list: 'Work' },
+        { list: 'Contact' }
+      ]
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="stylus">
+.header {
+  position: relative;
+
+  img {
+    width: 100%;
+  }
+
+  header {
+    width: 100%;
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 20px 120px;
+    color: #ffffff;
+    align-items: center;
+
+    h1 {
+      flex: 1;
+    }
+
+    ul {
+      display: flex;
+      list-style: none;
+      justify-content: flex-end;
+
+      li {
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
---
+++ ui/Slidshow.vue
@@ -0,0 +1,83 @@
+<template lang="pug">
+  .slideshow
+    .slideContents
+      section#slide1
+        img(src="img/top2.png")
+      section#slide2
+        img(src="img/top3.png")
+      section#slide3
+        img(src="img/top2.png")
+      section#slide4
+        img(src="img/top3.png")
+      section#slide5
+        img(src="img/top2.png")
+</template>
+
+
+<style lang="stylus">
+.slideshow {
+  position: relative;
+  overflow: hidden;
+}
+
+.slideContents {
+  position: relative;
+  text-align: center;
+  -webkit-user-select: none;
+  user-select: none;
+
+  section {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+  }
+}
+
+.slideContents section img {
+  max-width: 100%;
+  box-sizing: border-box;
+  vertical-align: middle;
+}
+
+.slideContents section {
+  animation: autoplay 30s cubic-bezier(0.215, 0.61, 0.355, 1) infinite;
+}
+
+@keyframes autoplay {
+  0%, 16%, 100% {
+    transform: none;
+  }
+
+  20% {
+    transform: translateX(-100%);
+  }
+
+  20.001%, 96% {
+    transform: translateX(100%);
+  }
+}
+
+.slideContents {
+  #slide1 {
+    position: relative;
+    animation-delay: 0s;
+  }
+
+  #slide2 {
+    animation-delay: -24s;
+  }
+
+  #slide3 {
+    animation-delay: -18s;
+  }
+
+  #slide4 {
+    animation-delay: -12s;
+  }
+
+  #slide5 {
+    animation-delay: -6s;
+  }
+}
+</style>
